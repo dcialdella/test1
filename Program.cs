cfaab6277e5f53a3293e6fb95c38fc74a01979ec
dcialdella <dcialdella@gmail.com>
2017-04-22 20:38:25 +0200

    A
    A

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//
+// algo aca
+//
+using System;
+
+namespace hwapp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var name = System.Environment.MachineName;
+            Console.WriteLine("!!! Hello World " + name + " !!!");
+        }
+    }
+}
+
+// test algo aca
